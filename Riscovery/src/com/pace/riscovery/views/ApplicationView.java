2197ac283d69e17980bca1117676494cebacfe77
Andrew Quartey <yestrup@gmail.com>
2014-10-17 22:44:37 +0000

    - fixed JTable: added Headings, changed font size, and started working on fitler

--- Riscovery/src/com/pace/riscovery/views/ApplicationView.java
+++ Riscovery/src/com/pace/riscovery/views/ApplicationView.java
@@ -1,5 +1,8 @@
 package com.pace.riscovery.views;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -20,9 +23,14 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
+import javax.swing.RowFilter;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 
-import com.pace.riscovery.DueDates;
 import com.pace.riscovery.MyRunnable;
 import com.pace.riscovery.Logic.Logic;
 
@@ -32,10 +40,46 @@ public class ApplicationView extends JFrame implements View{
 	private static final long serialVersionUID = -7360610421229722456L;
 	
 	Logic logic;
-	
+			    
 	private Action openAction, importAction, exitAction;
-	private JButton button1, button2;	
-		
+//	private JButton button1, button2;	
+	final JTextField filterText = new JTextField(20);
+	
+	private String[] columnNames = {"NO.", "INSURED", "CONTACT",
+        "EMAIL ADD.", "POSTAL ADD.", "COVER", "INSURER", "POLICY NO",
+        "COMM. DATE", "EXP. DATE", "PREM. CHARGED", "PREM. PAID", "PREM. O/S"
+        };
+
+	private Object[][] data = {
+	    {"1", "Smith",
+	     "0265324264", "yestrup@gmail.com", "P.O. Box GP, 4025 Accra", "Motor", "EIC", "DMC001", "12/10/2014", "17/10/2014",
+	     new Double(1200.95), new Double(133.67), new Double(42)},
+	     {"1", "Lobo",
+	     "0265324264", "yestrup@gmail.com", "P.O. Box GP, 4025 Accra", "House", "NIC", "DMC001", "12/10/2014", "17/10/2014",
+	     new Double(1200.95), new Double(133.67), new Double(42)}
+//	    {"John", "Doe",
+//	     "Rowing", new Integer(3), new Boolean(true)},
+//	    {"Sue", "Black",
+//	     "Knitting", new Integer(2), new Boolean(false)},
+//	    {"Jane", "White",
+//	     "Speed reading", new Integer(20), new Boolean(true)},
+//	    {"Joe", "Brown",
+//	     "Pool", new Integer(10), new Boolean(false)}
+	};
+	
+	TableModel model = new DefaultTableModel(data, columnNames) {
+	      public Class getColumnClass(int column) {
+	        Class returnValue;
+	        if ((column >= 0) && (column < getColumnCount())) {
+	          returnValue = getValueAt(0, column).getClass();
+	        } else {
+	          returnValue = Object.class;
+	        }
+	        return returnValue;
+	      }
+	    };
+	final TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(model);
+	
 	public ApplicationView(){
 		initActions();	
 		initMenu();
@@ -43,12 +87,12 @@ public class ApplicationView extends JFrame implements View{
 		initComponents();						
 	}
 		
-	private void initComponents() {		
-		GridBagConstraints c = new GridBagConstraints();
+	private void initComponents() {				
 		//SplitPane
 		JSplitPane split = new JSplitPane();
 		
 		//Left Component
+		GridBagConstraints c = new GridBagConstraints();
 		JPanel leftPanel = new JPanel(new GridBagLayout());
 		DefaultListModel<String> model = new DefaultListModel<String>();
 		model.addElement("Motor");
@@ -64,36 +108,34 @@ public class ApplicationView extends JFrame implements View{
 		leftPanel.add(listScroller, c);
 		
 		JButton button;
-		//Right component		
-		JPanel rightPanel = new JPanel(new GridBagLayout());
-		button = createButton1();
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridx = 5;
-		c.gridy = 0;
-		c.weightx = 0;
-		c.weighty = 0;
-//			c.gridwidth = ;
-		c.ipady = 5;
-		rightPanel.add(button, c);
-		
-		
-		button = createButton2();
-		c.gridx = 10;
-		c.gridy = 0;
-		c.gridwidth = 15;
-		rightPanel.add(button, c);
+		//Right componen		
+		JPanel rightPanel = new JPanel(new BorderLayout());		
+		button = createButton1();		
+		JPanel lowerPanel = new JPanel();
+		lowerPanel.add(button);		
+		button = createButton2();		
+		lowerPanel.add(button);		
+		button = createFilterButton();
+		lowerPanel.add(filterText);
+
+		lowerPanel.add(button);
+				
+		JScrollPane scrollPane = createTable();
+		rightPanel.add(scrollPane, BorderLayout.CENTER);		
+		rightPanel.add(lowerPanel, BorderLayout.PAGE_END);
 		
 		split.setLeftComponent(leftPanel);
 		split.setRightComponent(rightPanel);
-		this.add(split);					
+		add(split);
+		setSize(1000,400);
 	}
 
 	private void initToolbar() {
-		// TODO Auto-generated method stub		
+		
 	}
 
-	private JButton createButton1(){
-		button1 = new JButton("Check Expired Policies");
+	public JButton createButton1(){
+		JButton button1 = new JButton("Check Expired Policies");
 		button1.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
@@ -107,16 +149,43 @@ public class ApplicationView extends JFrame implements View{
 	
 	// This method is responsible for assignment button2 to the JList of expired/due policies
 	public JButton createButton2(){
-		button2 = new JButton("List of Due Polices");
+		JButton button2 = new JButton("List of Due Polices");
 		button2.addActionListener(new ActionListener(){
 			@Override
-			public void actionPerformed(ActionEvent arg0) {				
+			public void actionPerformed(ActionEvent arg0) {						
 				DueDates dueDate = new DueDates();  
 				dueDate.go();
 			}});
 		return button2;
 	}
 		
+	public JScrollPane createTable(){			
+		final JTable table = new JTable(model);
+	    table.setRowSorter(sorter);	    
+		table.setFont(new Font("Cambria", Font.PLAIN, 18));
+		JScrollPane scrollPane = new JScrollPane(table);
+		table.setAutoCreateRowSorter(true);
+		table.setPreferredScrollableViewportSize(new Dimension(1500, 600));
+        table.setFillsViewportHeight(true);
+		return scrollPane;
+	}
+	
+	public JButton createFilterButton(){
+		JButton button = new JButton("Filter");
+		button.addActionListener(new ActionListener() {
+	      public void actionPerformed(ActionEvent e) {
+	        String text = filterText.getText();
+	        if (text.length() == 0) {
+	          sorter.setRowFilter(null);
+	        } else {
+	          sorter.setRowFilter(RowFilter.regexFilter(text));
+	        }
+	      }
+	    });
+	return button;
+		
+	}
+	
 	private void initMenu(){
 		final JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
